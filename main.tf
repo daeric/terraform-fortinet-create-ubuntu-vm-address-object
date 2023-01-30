61cb14694b0a733ded449344831e0e59d2e3bf6f
Daeric <daeric@hashicorp.com>
2023-01-30 01:37:28 -0600

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    fortios = {
+      source = "fortinetdev/fortios"
+      version = "1.14.1"
+    }
+  }
+}
+
+provider "fortios" {
+  hostname = var.fortigate_ip
+  token    = var.fortigate_api_token
+  insecure = "true"
+}
+
+resource "fortios_firewall_address" "address" {
+  name       = var.vm_name
+  subnet     = var.ip_add
+  type       = "ipmask"
+  visibility = "enable"
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+
+variable "fortigate_ip" {
+  type = string
+}
+
+variable "fortigate_api_token" {
+  type = string
+}
+
+variable "ip_add" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
